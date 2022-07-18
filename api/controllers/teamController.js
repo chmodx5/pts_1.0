dd5e3f327adc261bc6b4aefd54ad07d96b94a53a
chmodx5 <chmodx5@gmail.com>
2022-07-18 22:52:46 +0300

    team controller

--- api/controllers/teamController.js
+++ api/controllers/teamController.js
@@ -1,27 +1,52 @@
 const db = require("../models");
 
+const Team = db.team;
+
 //creating main controller
 const createTeam = async (req, res) => {
-  res.json({ message: "create Team" });
+  let teamToBeCreated = {
+    Name: req.body.Name,
+    Location: req.body.Location,
+    TeamLeaderId: req.body.TeamLeaderId,
+    IsExternal: req.body.IsExternal,
+  };
+  const team = await Team.create(teamToBeCreated);
+
+  res.json(team);
 };
 
 //get all Teams
 const getAllTeams = async (req, res) => {
-  res.json({ message: "All Teams" });
+  let teams = await Team.findAll({});
+  res.json(teams);
 };
 
 //get Team by id
 const getTeamById = async (req, res) => {
-  res.json({ message: "get Team by id" });
+  let id = req.params.id;
+  let team = await Team.findOne({ where: { id: id } });
+  res.json(team);
 };
 
 //update Team
 const updateTeam = async (req, res) => {
-  res.json({ message: "update Team" });
+  let id = req.params.id;
+  let teamToBeUpdated = {
+    Name: req.body.Name,
+    Location: req.body.Location,
+    TeamLeaderId: req.body.TeamLeaderId,
+    IsExternal: req.body.IsExternal,
+  };
+  const team = await Team.update(teamToBeUpdated, { where: { id: id } });
+
+  res.status(200).send(product);
+  res.json(team);
 };
 
 //delete Team
 const deleteTeam = async (req, res) => {
+  let id = req.params.id;
+  await Team.destroy({ where: { id: id } });
   res.json({ message: "Team deleted successfully" });
 };
 
