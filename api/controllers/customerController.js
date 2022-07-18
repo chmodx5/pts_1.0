776d6b4e4ea9dce8fb7e8a097daf1cc58f1669f5
su-sid <davidsudi20@gmail.com>
2022-07-19 00:08:04 +0300

    first-commit

--- api/controllers/customerController.js
+++ api/controllers/customerController.js
@@ -1,27 +1,56 @@
 const db = require("../models");
 
+const Customer = db.customer;
+
 //creating main controller
 const createCustomer = async (req, res) => {
-  res.json({ message: "create Customer" });
+
+let customerToBeCreated ={
+  Name: req.body.Name,
+    Location: req.body.Location,
+    TeamLeaderId: req.body.TeamLeaderId,
+    IsExternal: req.body.IsExternal,
+}
+const customer= await Customer.create(customerToBeCreated);
+  res.json(customer);
 };
 
 //get all Customers
 const getAllCustomers = async (req, res) => {
-  res.json({ message: "All Customers" });
+  let customer = await Customer.findAll({});
+  res.json(customer);
 };
+
 
 //get Customer by id
 const getCustomerById = async (req, res) => {
-  res.json({ message: "get Customer by id" });
+  let id = req.params.id;
+  let customer = await Customer.findOne({ where: { id: id } });
+  res.json(customer);
 };
 
 //update Customer
 const updateCustomer = async (req, res) => {
-  res.json({ message: "update Customer" });
+ 
+  let id = req.params.id;
+  let customerToBeUpdated = {
+    Name: req.body.Name,
+    Location: req.body.Location,
+    TeamLeaderId: req.body.TeamLeaderId,
+    IsExternal: req.body.IsExternal,
+  };
+  const customer = await Customer.update(customerToBeUpdated, { where: { id: id } });
+
+  res.status(200).send(customer);
+
+  res.json(customer);
 };
 
 //delete Customer
 const deleteCustomer = async (req, res) => {
+
+  let id = req.params.id;
+  await Customer.destroy({ where: { id: id } });
   res.json({ message: "Customer deleted successfully" });
 };
 
--- api/controllers/personController.js
+++ api/controllers/personController.js
@@ -1,27 +1,61 @@
 const db = require("../models");
 
+
+const Person = db.person;
+
+
 //creating main controller
 const createPerson = async (req, res) => {
-  res.json({ message: "create Person" });
+
+
+  let personToBeCreated ={
+    Name: req.body.Name,
+      Location: req.body.Location,
+      TeamLeaderId: req.body.TeamLeaderId,
+      IsExternal: req.body.IsExternal,
+  }
+  const person= await Person.create(personToBeCreated);
+    res.json(person);
 };
 
 //get all Persons
 const getAllPersons = async (req, res) => {
-  res.json({ message: "All Persons" });
+
+  let person = await Person.findAll({});
+  res.json(person);
 };
 
 //get Person by id
 const getPersonById = async (req, res) => {
-  res.json({ message: "get Person by id" });
+
+  let id = req.params.id;
+  let person = await Person.findOne({ where: { id: id } });
+  res.json(person);
 };
 
 //update Person
 const updatePerson = async (req, res) => {
-  res.json({ message: "update Person" });
+
+  let id = req.params.id;
+  let personToBeUpdated = {
+    Name: req.body.Name,
+    Location: req.body.Location,
+    TeamLeaderId: req.body.TeamLeaderId,
+    IsExternal: req.body.IsExternal,
+  };
+  const person = await Person.update(personToBeUpdated, { where: { id: id } });
+
+  res.status(200).send(person);
+
+  res.json(person);
 };
 
 //delete Person
 const deletePerson = async (req, res) => {
+
+  let id = req.params.id;
+  await Customer.destroy({ where: { id: id } });
+
   res.json({ message: "Person deleted successfully" });
 };
 
