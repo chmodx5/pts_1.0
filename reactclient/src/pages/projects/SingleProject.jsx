3910ffb6b692946ceb35bf34b10c933801859562
chmodx5 <chmodx5@gmail.com>
2022-07-22 06:48:50 +0300

    created components for the tasks and added heroincons

--- reactclient/src/pages/projects/SingleProject.jsx
+++ reactclient/src/pages/projects/SingleProject.jsx
@@ -1,9 +1,51 @@
+import { Tab } from "@headlessui/react";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Card from "../../components/styled/Card";
 import axios from "axios";
+import TasksList from "../../components/tasks/TasksList";
+import AddTask from "../../components/tasks/AddTask";
+import SelectedProjectDetails from "../../components/tasks/SelectedProjectDetails";
 
-const SingleProject = () => {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function SingleProject() {
+  let [categories] = useState({
+    "Create Tasks": [
+      {
+        id: 1,
+        title: "Does drinking coffee make you smarter?",
+        date: "5h ago",
+        commentCount: 5,
+        shareCount: 2,
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: "2h ago",
+        commentCount: 3,
+        shareCount: 2,
+      },
+    ],
+    Tasks: [
+      {
+        id: 1,
+        title: "Is tech making coffee better or worse?",
+        date: "Jan 7",
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: "The most innovative things happening in coffee",
+        date: "Mar 19",
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+  });
   const [project, setProject] = useState({});
 
   const { id } = useParams();
@@ -17,30 +59,57 @@ const SingleProject = () => {
       })
       .catch((err) => console.log(err));
   }, []);
-
   return (
-    <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-xl">
-      <h1 className="font-bold text-center text-2xl mb-5 capitalize">
-        {project.Name}
-      </h1>
-      <Card className=" divide-gray-200">
-        <div className="px-5 py-7">
-          <h1>{project.Name} - Tasks</h1>
-        </div>
-        <div className="px-6 py-4 grid grid-cols-2">
-          <div className="">
-            <h3>Add task</h3>
-            <ul>
-              <li>tasks</li>
-            </ul>
+    <div className="w-full">
+      <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-xl">
+        <Card className=" divide-gray-200 ">
+          <div className="px-5 py-7">
+            <h1>{project.Name} - Tasks</h1>
+            <SelectedProjectDetails project={project} />
           </div>
-          <div>
-            <h3>All Tasks</h3>
+          <div className="px-6 py-4 ">
+            <Tab.Group>
+              <Tab.List className="flex space-x-1 rounded-xl  p-1">
+                {Object.keys(categories).map((category) => (
+                  <Tab
+                    key={category}
+                    className={({ selected }) =>
+                      classNames(
+                        "w-full rounded-lg py-2.5 text-sm font-bold leading-5",
+                        "ring-white ring-opacity-60 ring-offset-2 ring-offset-primary focus:outline-none focus:ring-2",
+                        selected
+                          ? "bg-primary  text-white shadow"
+                          : " hover:bg-white/[0.12] hover:text-primary"
+                      )
+                    }
+                  >
+                    {category}
+                  </Tab>
+                ))}
+              </Tab.List>
+              <Tab.Panels className="mt-2">
+                <Tab.Panel
+                  className={classNames(
+                    "rounded-xl bg-white p-3",
+                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                  )}
+                >
+                  <AddTask />
+                </Tab.Panel>
+
+                <Tab.Panel
+                  className={classNames(
+                    "rounded-xl bg-white p-3",
+                    "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2"
+                  )}
+                >
+                  <TasksList />
+                </Tab.Panel>
+              </Tab.Panels>
+            </Tab.Group>
           </div>
-        </div>
-      </Card>
+        </Card>
+      </div>
     </div>
   );
-};
-
-export default SingleProject;
+}
