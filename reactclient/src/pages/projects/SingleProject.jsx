4a0a698723d5fe256e3f92aa926cd96c55a39a48
su-sid <davidsudi20@gmail.com>
2022-07-23 16:56:33 +0300

    second commit

--- reactclient/src/pages/projects/SingleProject.jsx
+++ reactclient/src/pages/projects/SingleProject.jsx
@@ -14,36 +14,11 @@ function classNames(...classes) {
 export default function SingleProject() {
   let [categories] = useState({
     "Create Tasks": [
-      {
-        id: 1,
-        title: "Does drinking coffee make you smarter?",
-        date: "5h ago",
-        commentCount: 5,
-        shareCount: 2,
-      },
-      {
-        id: 2,
-        title: "So you've bought coffee... now what?",
-        date: "2h ago",
-        commentCount: 3,
-        shareCount: 2,
-      },
+
+    
     ],
     Tasks: [
-      {
-        id: 1,
-        title: "Is tech making coffee better or worse?",
-        date: "Jan 7",
-        commentCount: 29,
-        shareCount: 16,
-      },
-      {
-        id: 2,
-        title: "The most innovative things happening in coffee",
-        date: "Mar 19",
-        commentCount: 24,
-        shareCount: 12,
-      },
+
     ],
   });
   const [project, setProject] = useState({});
